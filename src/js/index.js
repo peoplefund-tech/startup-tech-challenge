fda4474bdbc4577d023643b2053b9bc1dc0c32fc
n0rr7882 <n0rr7882@gmail.com>
2019-08-16 04:49:41 +0900

    연사모집 배너 추가

--- src/js/index.js
+++ src/js/index.js
@@ -1,12 +1,1 @@
-import $ from 'jquery';
-
-const SPEAKER_RECRUITMENT = '이 세션을 빛내주세요!';
-const SPEAKER_RECRUITMENT_HOVER = '연사 신청하기';
-
-$('#btn-speaker-recruitment').hover(e => {
-    $(e.target).text(SPEAKER_RECRUITMENT_HOVER);
-    $(e.target).addClass('pfont-b');
-}, e => {
-    $(e.target).text(SPEAKER_RECRUITMENT);
-    $(e.target).removeClass('pfont-b');
-});
+console.log('Hello, peoplefund!');
